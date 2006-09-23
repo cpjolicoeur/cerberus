b82800f56967f8d324f3c3504ac9b4d2dd6ff189
anatol <anatol@65aa75ef-ce15-0410-bc34-cdc86d5f77e6>
2006-09-23 10:07:19 +0000

    Simplify RSS test
    git-svn-id: svn+ssh://rubyforge.org/var/svn/cerberus@91 65aa75ef-ce15-0410-bc34-cdc86d5f77e6

--- test/rss_publisher_test.rb
+++ test/rss_publisher_test.rb
@@ -6,19 +6,7 @@ require 'tempfile'
 
 class RSSPublisherTest < Test::Unit::TestCase
   def test_publisher
-    rss_file = tf = Tempfile.new('cerberus-rss')
-    options = Cerberus::Config.new(nil, :publisher => {:rss => {:file => rss_file.path}}, :application_name => 'RSSApp')
-    build = DummyManager.new('last message', 'this is output', 1235, 'anatol')
-
-    Cerberus::Publisher::RSS.publish(:setup, build, options)
-
-    xml = REXML::Document.new(IO.read(rss_file.path))
-
-    assert_equal '[RSSApp] Cerberus set up for project (#1235)', xml.elements["rss/channel/item/title/"].get_text.value
-  end
-  
-  def test_publisher_xml_in_body_and_title
-    rss_file = tf = Tempfile.new('cerberus-rss')
+    rss_file = Tempfile.new('cerberus-rss')
     options = Cerberus::Config.new(nil, :publisher => {:rss => {:file => rss_file.path}}, :application_name => 'RSS<App')
     build = DummyManager.new('last message', 'this is output', 1235, 'anatol')
 
@@ -27,6 +15,7 @@ class RSSPublisherTest < Test::Unit::TestCase
     xml = REXML::Document.new(IO.read(rss_file.path))
 
     assert_equal '[RSS<App] Cerberus set up for project (#1235)', xml.elements["rss/channel/item/title/"].get_text.value
-    assert_match %r{<pre>last message\n\nthis is output\n\n--\nCerberus 0.\d.\d, http://cerberus.rubyforge.com/</pre>}, xml.elements["rss/channel/item/description/"].get_text.value
+    assert_match %r{<pre>last message\n\nthis is output\n\n--\nCerberus 0.\d.\d, http://cerberus.rubyforge.com/</pre>}, 
+                     xml.elements["rss/channel/item/description/"].get_text.value
   end
 end
