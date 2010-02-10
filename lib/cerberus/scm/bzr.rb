473174b57078b0611716c81e0c90d04ff3aea8bd
Paul Hinze <paul.t.hinze@gmail.com>
2010-02-10 15:55:13 -0600

    Fix regexp to recognize revno from merged revisions

--- lib/cerberus/scm/bzr.rb
+++ lib/cerberus/scm/bzr.rb
@@ -64,7 +64,7 @@ class Cerberus::SCM::Bazaar
     # message:
     #   sidfugsdiufgsdifusdg
  
-    @revision = lastlog.match(/^revno: (\d+)$/)[1].to_i
+    @revision = lastlog.match(/^revno: (\d+).*$/)[1].to_i
     @author   = lastlog.match(/^committer: (.+)$/)[1]
     @date     = Time.parse(lastlog.match(/^timestamp: (.+)$/)[1])
     @message  = lastlog.match(/message:\n  (.*)/m)[1]
