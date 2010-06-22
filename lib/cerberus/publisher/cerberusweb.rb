48b7808c71773994f4f8e5a1537d6f2c3fabc32a
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2010-06-22 11:22:59 -0400

    add base cerberusweb publisher file and test

---
+++ lib/cerberus/publisher/cerberusweb.rb
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'cerberus/publisher/base'
+
+class Cerberus::Publisher::CerberusWeb < Cerberus::Publisher::Base
+  def self.publish(state, manager, options)
+    # TODO: replace with actual publisher content
+  end
+end
---
+++ test/cerberusweb_publisher_test.rb
@@ -0,0 +1,12 @@
+require File.dirname(__FILE__) + '/test_helper'
+
+require 'cerberus/publisher/cerberusweb'
+require 'cerberus/manager'
+require 'mock/manager'
+
+class CerberuswebPublisherText < Test::Unit::TestCase
+  def test_publisher
+    # TODO: replace with real tests
+    assert true
+  end
+end
